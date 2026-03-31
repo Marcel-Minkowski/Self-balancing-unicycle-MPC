bb622528048e77cad6422818c7e4f98f7866d114
Marcel Minkowski <marcelminkowski04@gmail.com>
2026-03-31 15:50:54 +0200

    Added MPC file

---
+++ MPC.py
@@ -0,0 +1,9 @@
+import sympy as sm
+import sympy.physics.mechanics as me
+import numpy as np
+#import scipy
+from scipy.linalg import null_space
+import Test_numerical_solution
+
+np.set_printoptions(precision=5, suppress=True)
+
